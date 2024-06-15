2d808bfcde4dd396660d910e05cb889d030046ea
vsiagoCodeCity <iago@codecity.app>
2024-06-14 21:37:13 -0300

    mobile

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -13,93 +13,104 @@ export default function Home() {
 
       <section className="h-[88vh] -mb-1 relative  flex items-center justify-center bg-gradient-to-tr from-[#311818] via-[#BE2B4E] via-70% to-[#8A0000]/60">
         <div className="background-indian-texture  h-full w-full">
-        <div className="container  flex items-center flex-col h-full md:flex-row">
-          <div className=" h-[30%] md:h-full md:w-[50%] flex items-end justify-end md:order-2 ">
-            <Image
-              width={700}
-              height={700}
-              src="/menina-art-1.png"
-              alt="Algo"
-              className="h-[90%] w-full object-contain"
-              objectFit="cover"
-            />
-          </div>
-          <div className=" h-[50%] md:order-1 md:w-[50%]">
-            <span className="text-center block font-sans font-semibold text-xs tracking-[4px] mb-3 text-[#ff7a99] md:text-start md:text-xl">
-              BEM-VINDO AO NOSSO
-            </span>
-            <h1 className="text-center font-life-savers text-4xl mb-3 text-white font-black md:text-[6rem] md:leading-[90px] md:text-start">
-              Instituto Social Bezerra de Menezes
-            </h1>
-            <p className="font-lato text-lg text-center mx-12 md:mx-0 leading-5 tracking-wide md:text-start md:text-3xl md:pr-32 text-[#D8A0A0]">
-               Transformando vidas através da, cultura e da arte 🎨💃
-            </p>
-            <div className="flex gap-x-3 items-center justify-center mt-3 md:justify-start md:mt-6">
-              <a href="/" className="block text-#E0234E px-6 py-4 bg-[#E0234E] rounded-full text-white font-bold">APOIAR</a>
-              <a href="/" className="block text-white font-bold px-6 py-4 border-2 border-[#E0234E] rounded-full">Quem Somos</a>
+          <div className="container flex items-center justify-between flex-col h-full md:flex-row">
+            <div className=" h-[45%] md:h-full w-full md:w-[50%] flex items-end justify-end order-1 md:order-2">
+              <Image
+                width={700}
+                height={700}
+                src="/menina-art-1.png"
+                alt="Algo"
+                className="h-full md:h-[90%] w-full object-contain"
+                objectFit="cover"
+              />
+            </div>
+
+            <div className=" h-[55%] md:order-1 md:w-[50%] flex flex-col items-start justify-end">
+              <span className="mx-auto md:mx-0 text-center block   text-xs tracking-[4px] mb-3 text-[#ffbecd] font-bai-jamjuree font-bold md:text-start md:text-xl">
+                BEM-VINDO AO NOSSO
+              </span>
+              <h1 className="text-center font-life-savers text-4xl mb-3 text-white font-black md:text-[6rem] md:leading-[90px] md:text-start">
+                Instituto Social Bezerra de Menezes
+              </h1>
+              <p className="font-lato text-lg text-center mx-12 md:mx-0 leading-5  md:text-start md:text-3xl md:pr-32 text-[#D8A0A0]">
+                Transformando vidas através da, cultura e da arte 🎨💃
+              </p>
+              <div className="flex gap-x-3 items-center justify-center mt-3 md:justify-start md:mt-6 w-full">
+                <a
+                  href="/"
+                  className="block text-#E0234E px-5 md:px-6 py-3 md:py-4 bg-[#E0234E] rounded-full text-white font-bold"
+                >
+                  APOIAR
+                </a>
+                <a
+                  href="/"
+                  className="block text-white font-bold px-5 md:px-6 py-3 md:py-4 border-2 border-[#E0234E] rounded-full"
+                >
+                  Quem Somos
+                </a>
+              </div>
             </div>
           </div>
-        </div>
-        <div class="custom-shape-divider-bottom-1718321215">
-          <svg
-            data-name="Layer 1"
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 1200 120"
-            preserveAspectRatio="none"
-          >
-            <defs>
-              <linearGradient id="gradient" x1="100%" y1="0%" x2="0%" y2="0%">
-                <stop offset="0%" stop-color="#F0E0E0" />
-                <stop offset="50%" stop-color="#F0E0E0" />
-                <stop offset="100%" stop-color="#F0E0E0" />
-              </linearGradient>
-            </defs>
+          <div class="custom-shape-divider-bottom-1718321215">
+            <svg
+              data-name="Layer 1"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 1200 120"
+              preserveAspectRatio="none"
+            >
+              <defs>
+                <linearGradient id="gradient" x1="100%" y1="0%" x2="0%" y2="0%">
+                  <stop offset="0%" stop-color="#F0E0E0" />
+                  <stop offset="50%" stop-color="#F0E0E0" />
+                  <stop offset="100%" stop-color="#F0E0E0" />
+                </linearGradient>
+              </defs>
 
-            <path
-              d="M0,0V46.29c47.79,22.2,103.59,32.17,158,28,70.36-5.37,136.33-33.31,206.8-37.5C438.64,32.43,512.34,53.67,583,72.05c69.27,18,138.3,24.88,209.4,13.08,36.15-6,69.85-17.84,104.45-29.34C989.49,25,1113-14.29,1200,52.47V0Z"
-              opacity=".25"
-              fill="url(#gradient)"
-            ></path>
-            <path
-              d="M0,0V15.81C13,36.92,27.64,56.86,47.69,72.05,99.41,111.27,165,111,224.58,91.58c31.15-10.15,60.09-26.07,89.67-39.8,40.92-19,84.73-46,130.83-49.67,36.26-2.85,70.9,9.42,98.6,31.56,31.77,25.39,62.32,62,103.63,73,40.44,10.79,81.35-6.69,119.13-24.28s75.16-39,116.92-43.05c59.73-5.85,113.28,22.88,168.9,38.84,30.2,8.66,59,6.17,87.09-7.5,22.43-10.89,48-26.93,60.65-49.24V0Z"
-              opacity=".5"
-              fill="url(#gradient)"
-            ></path>
-            <path
-              d="M0,0V5.63C149.93,59,314.09,71.32,475.83,42.57c43-7.64,84.23-20.12,127.61-26.46,59-8.63,112.48,12.24,165.56,35.4C827.93,77.22,886,95.24,951.2,90c86.53-7,172.46-45.71,248.8-84.81V0Z"
-              fill="url(#gradient)"
-            ></path>
-          </svg>
-          <svg
-            data-name="Layer 1"
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 1200 120"
-            preserveAspectRatio="none"
-          >
-            <defs>
-              <linearGradient id="gradient" x1="100%" y1="0%" x2="0%" y2="0%">
-                <stop offset="0%" stop-color="#F0E0E0" />
-                <stop offset="50%" stop-color="#F0E0E0" />
-                <stop offset="100%" stop-color="#F0E0E0" />
-              </linearGradient>
-            </defs>
+              <path
+                d="M0,0V46.29c47.79,22.2,103.59,32.17,158,28,70.36-5.37,136.33-33.31,206.8-37.5C438.64,32.43,512.34,53.67,583,72.05c69.27,18,138.3,24.88,209.4,13.08,36.15-6,69.85-17.84,104.45-29.34C989.49,25,1113-14.29,1200,52.47V0Z"
+                opacity=".25"
+                fill="url(#gradient)"
+              ></path>
+              <path
+                d="M0,0V15.81C13,36.92,27.64,56.86,47.69,72.05,99.41,111.27,165,111,224.58,91.58c31.15-10.15,60.09-26.07,89.67-39.8,40.92-19,84.73-46,130.83-49.67,36.26-2.85,70.9,9.42,98.6,31.56,31.77,25.39,62.32,62,103.63,73,40.44,10.79,81.35-6.69,119.13-24.28s75.16-39,116.92-43.05c59.73-5.85,113.28,22.88,168.9,38.84,30.2,8.66,59,6.17,87.09-7.5,22.43-10.89,48-26.93,60.65-49.24V0Z"
+                opacity=".5"
+                fill="url(#gradient)"
+              ></path>
+              <path
+                d="M0,0V5.63C149.93,59,314.09,71.32,475.83,42.57c43-7.64,84.23-20.12,127.61-26.46,59-8.63,112.48,12.24,165.56,35.4C827.93,77.22,886,95.24,951.2,90c86.53-7,172.46-45.71,248.8-84.81V0Z"
+                fill="url(#gradient)"
+              ></path>
+            </svg>
+            <svg
+              data-name="Layer 1"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 1200 120"
+              preserveAspectRatio="none"
+            >
+              <defs>
+                <linearGradient id="gradient" x1="100%" y1="0%" x2="0%" y2="0%">
+                  <stop offset="0%" stop-color="#F0E0E0" />
+                  <stop offset="50%" stop-color="#F0E0E0" />
+                  <stop offset="100%" stop-color="#F0E0E0" />
+                </linearGradient>
+              </defs>
 
-            <path
-              d="M0,0V46.29c47.79,22.2,103.59,32.17,158,28,70.36-5.37,136.33-33.31,206.8-37.5C438.64,32.43,512.34,53.67,583,72.05c69.27,18,138.3,24.88,209.4,13.08,36.15-6,69.85-17.84,104.45-29.34C989.49,25,1113-14.29,1200,52.47V0Z"
-              opacity=".25"
-              fill="url(#gradient)"
-            ></path>
-            <path
-              d="M0,0V15.81C13,36.92,27.64,56.86,47.69,72.05,99.41,111.27,165,111,224.58,91.58c31.15-10.15,60.09-26.07,89.67-39.8,40.92-19,84.73-46,130.83-49.67,36.26-2.85,70.9,9.42,98.6,31.56,31.77,25.39,62.32,62,103.63,73,40.44,10.79,81.35-6.69,119.13-24.28s75.16-39,116.92-43.05c59.73-5.85,113.28,22.88,168.9,38.84,30.2,8.66,59,6.17,87.09-7.5,22.43-10.89,48-26.93,60.65-49.24V0Z"
-              opacity=".5"
-              fill="url(#gradient)"
-            ></path>
-            <path
-              d="M0,0V5.63C149.93,59,314.09,71.32,475.83,42.57c43-7.64,84.23-20.12,127.61-26.46,59-8.63,112.48,12.24,165.56,35.4C827.93,77.22,886,95.24,951.2,90c86.53-7,172.46-45.71,248.8-84.81V0Z"
-              fill="url(#gradient)"
-            ></path>
-          </svg>
-        </div>
+              <path
+                d="M0,0V46.29c47.79,22.2,103.59,32.17,158,28,70.36-5.37,136.33-33.31,206.8-37.5C438.64,32.43,512.34,53.67,583,72.05c69.27,18,138.3,24.88,209.4,13.08,36.15-6,69.85-17.84,104.45-29.34C989.49,25,1113-14.29,1200,52.47V0Z"
+                opacity=".25"
+                fill="url(#gradient)"
+              ></path>
+              <path
+                d="M0,0V15.81C13,36.92,27.64,56.86,47.69,72.05,99.41,111.27,165,111,224.58,91.58c31.15-10.15,60.09-26.07,89.67-39.8,40.92-19,84.73-46,130.83-49.67,36.26-2.85,70.9,9.42,98.6,31.56,31.77,25.39,62.32,62,103.63,73,40.44,10.79,81.35-6.69,119.13-24.28s75.16-39,116.92-43.05c59.73-5.85,113.28,22.88,168.9,38.84,30.2,8.66,59,6.17,87.09-7.5,22.43-10.89,48-26.93,60.65-49.24V0Z"
+                opacity=".5"
+                fill="url(#gradient)"
+              ></path>
+              <path
+                d="M0,0V5.63C149.93,59,314.09,71.32,475.83,42.57c43-7.64,84.23-20.12,127.61-26.46,59-8.63,112.48,12.24,165.56,35.4C827.93,77.22,886,95.24,951.2,90c86.53-7,172.46-45.71,248.8-84.81V0Z"
+                fill="url(#gradient)"
+              ></path>
+            </svg>
+          </div>
         </div>
       </section>
       <section className="-mt-[2px] h-96 bg-[#F0E0E0]"></section>
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,93 +1,135 @@
-"use client"
+"use client";
 import { useState, useEffect } from "react";
 import Image from "next/image";
 import Link from "next/link";
 
 export default function Header() {
-    const [open, setOpen] = useState(false);
-    const [scrolled, setScrolled] = useState(false);
-    const [prevScrollPos, setPrevScrollPos] = useState(0);
+  const [open, setOpen] = useState(false);
+  const [scrolled, setScrolled] = useState(false);
+  const [prevScrollPos, setPrevScrollPos] = useState(0);
 
-    const menuItems = [
-        {
-            nome: "Início",
-            link: "/"
-        },
-        {
-            nome: "Doar",
-            link: "/doar"
-        },
-    ];
+  const menuItems = [
+    {
+      nome: "Início",
+      link: "/",
+    },
+    {
+      nome: "Doar",
+      link: "/doar",
+    },
+  ];
 
-    useEffect(() => {
-        const handleScroll = () => {
-            const currentScrollPos = window.pageYOffset;
-            const isScrolled = currentScrollPos > 0;
+  useEffect(() => {
+    const handleScroll = () => {
+      const currentScrollPos = window.pageYOffset;
+      const isScrolled = currentScrollPos > 0;
 
-            setScrolled(isScrolled);
+      setScrolled(isScrolled);
 
-            if (currentScrollPos > prevScrollPos) {
-                // Scrolling down
-                setPrevScrollPos(currentScrollPos);
-                setOpen(false);
-            } else {
-                // Scrolling up
-                setPrevScrollPos(currentScrollPos);
-            }
-        };
+      if (currentScrollPos > prevScrollPos) {
+        // Scrolling down
+        setPrevScrollPos(currentScrollPos);
+        setOpen(false);
+      } else {
+        // Scrolling up
+        setPrevScrollPos(currentScrollPos);
+      }
+    };
 
-        window.addEventListener("scroll", handleScroll);
-        return () => {
-            window.removeEventListener("scroll", handleScroll);
-        };
-    }, [prevScrollPos]);
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, [prevScrollPos]);
 
-    return (
-        <header className={`${scrolled ? "bg-[#E0234E] fixed top-0 z-10 h-16 flex w-full translate-y-0 duration-300 ease-in-out bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-90" : "h-40 bg-gradient-to-r w-full from-[#140D10]/0 absolute top-0 z-20 left-0 via-[#621628]/0 to-[#621628]/0"} `}>
-            <nav className="min-h-full px-6 flex w-full justify-between items-center md:container mx-auto ">
-                <a href="/">
-                    <Image
-                        src="/logo-isbm.svg"
-                        width={150}
-                        height={20}
-                        alt="Logo Rubao"
-                    />
-                </a>
-                <ul
-                    onClick={() => setOpen(!open)}
-                    className="md:hidden relative flex h-10 w-10 flex-col items-center justify-center gap-3 cursor-pointer hover:bg-[#E0234E] rounded transition duration-150 ease-in hover:transform hover:scale-105"
-                >
-                    <li
-                        className={`${open ? "transform rotate-45 absolute" : ""} w-7 h-[3px] rounded-[2px] bg-white transition duration-150 ease-in-out`}
-                    ></li>
-                    <li
-                        className={`${open ? "transform -rotate-45 absolute" : ""} w-7 h-[3px] rounded-[2px] bg-white transition duration ease-in-out`}
-                    ></li>
-                </ul>
-                <ul
-                    className={`${open ? "bg-gradient-to-r from-[#140D10]/60 via-[#621628] to-[#621628]/70 flex opacity-100 w-full h-screen left-0 z-[50] backdrop-blur-lg mt-4" : "w-full h-0"} fixed ${scrolled ? "top-16" : "top-20"} right-0 flex flex-col transition duration-200 ease-out opacity-0 flex-1`}
-                >
-                    {menuItems.map((item, index) => (
-                        <li
-                            key={item.nome}
-                            style={{ transitionDelay: `${open ? index * 70 : 0}ms` }}
-                            className={`px-6 ${open ? 'opacity-100 transform translate-x-0 ' : 'opacity-0 transform -translate-x-10'} transition-all ease-out duration-500`}
-                        >
-                            <a
-                                className={`${open ? 'inline-block ml-0 pointer-events-auto transition ease-in py-6' : 'py-0 opacity-0 ml-6 hidden pointer-events-none'} text-red-200 border-b-[1px] border-red-950 w-full focus:text-white focus:font-semibold`}
-                                href={`${item.link}`}
-                            >
-                                {item.nome}
-                            </a>
-                        </li>
-                    ))}
-                </ul>
-                <ul className="hidden md:flex gap-6 items-center justify-center">{menuItems.map((item, intex) => (
-                    <li className={`${item.nome === "Doar" ? 'bg-[#E0234E] p-3 px-6 text-white rounded-full hover:scale-110 cursor-pointer transition-all ease-in-out  hover:shadow-2xl hover:drop-shadow-2xl hover:bg-red-600/40 hover:border-4 hover:border-red-500' : ''}`} key={item.nome}>
-                        <Link className={`${scrolled ? 'text-text-white' : ''} h-20 px-2 py-1 text-white hover:text-white  text-xl active:text-red-400 ${item.nome === "Doar" ? 'text-white font-bold' : ''}`} href={item.link}>{item.nome}</Link>
-                    </li>
-                ))}</ul>
-            </nav>
-        </header>
-    );
+  return (
+    <header
+      className={`${
+        scrolled
+          ? "bg-[#E0234E] fixed top-0 z-10 h-16 flex w-full translate-y-0 duration-300 ease-in-out bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-90"
+          : "py-4 md:py-0 md:h-40 bg-gradient-to-r w-full from-[#140D10]/0 absolute top-0 z-20 left-0 via-[#621628]/0 to-[#621628]/0"
+      } `}
+    >
+      <nav className="min-h-full px-6 flex w-full justify-between items-center md:container mx-auto ">
+        <a href="/">
+          <Image
+            src="/logo-isbm.svg"
+            width={150}
+            height={20}
+            alt="Logo Rubao"
+          />
+        </a>
+        <ul
+          onClick={() => setOpen(!open)}
+          className="md:hidden relative flex h-10 w-10 flex-col items-center justify-center gap-3 cursor-pointer hover:bg-[#E0234E] rounded transition duration-150 ease-in hover:transform hover:scale-105"
+        >
+          <li
+            className={`${
+              open ? "transform rotate-45 absolute" : ""
+            } w-7 h-[3px] rounded-[2px] bg-white transition duration-150 ease-in-out`}
+          ></li>
+          <li
+            className={`${
+              open ? "transform -rotate-45 absolute" : ""
+            } w-7 h-[3px] rounded-[2px] bg-white transition duration ease-in-out`}
+          ></li>
+        </ul>
+        <ul
+          className={`${
+            open
+              ? "bg-gradient-to-r from-[#140D10]/60 via-[#621628] to-[#621628]/70 flex opacity-100 w-full h-screen left-0 z-[50] backdrop-blur-lg mt-4"
+              : "w-full h-0"
+          } fixed ${
+            scrolled ? "top-16" : "top-20"
+          } right-0 flex flex-col transition duration-200 ease-out opacity-0 flex-1`}
+        >
+          {menuItems.map((item, index) => (
+            <li
+              key={item.nome}
+              style={{ transitionDelay: `${open ? index * 70 : 0}ms` }}
+              className={`px-6 ${
+                open
+                  ? "opacity-100 transform translate-x-0 "
+                  : "opacity-0 transform -translate-x-10"
+              } transition-all ease-out duration-500`}
+            >
+              <a
+                className={`${
+                  open
+                    ? "inline-block ml-0 pointer-events-auto transition ease-in py-6"
+                    : "py-0 opacity-0 ml-6 hidden pointer-events-none"
+                } text-red-200 border-b-[1px] border-red-950 w-full focus:text-white focus:font-semibold`}
+                href={`${item.link}`}
+              >
+                {item.nome}
+              </a>
+            </li>
+          ))}
+        </ul>
+        <ul className="hidden md:flex gap-6 items-center justify-center">
+          {menuItems.map((item, intex) => (
+            <li
+              className={`${
+                item.nome === "Doar"
+                  ? "bg-[#E0234E] p-3 px-6 text-white rounded-full hover:scale-110 cursor-pointer transition-all ease-in-out  hover:shadow-2xl hover:drop-shadow-2xl hover:bg-red-600/40 hover:border-4 hover:border-red-500"
+                  : ""
+              }`}
+              key={item.nome}
+            >
+              <Link
+                className={`${
+                  scrolled ? "text-text-white" : ""
+                } h-20 px-2 py-1 text-white hover:text-white  text-xl active:text-red-400 ${
+                  item.nome === "Doar" ? "text-white font-bold" : ""
+                }`}
+                href={item.link}
+              >
+                {item.nome}
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </nav>
+    </header>
+  );
 }
