da4740e9ce975054e09ca93dfc806a96d92930ce
vsiago <vsiago22@gmail.com>
2024-07-09 16:29:20 -0300

    melhorias

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -24,8 +24,6 @@ export default function Header({changeHeaderBgColor}: HeaderProps) {
     },
   ];
 
-
-
   useEffect(() => {
     const handleScroll = () => {
         const currentScrollPos = window.pageYOffset;
